f10806b99cd4a16df41f4cfbea8eb49281243aab
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-04 16:32:42 +0000

    Snapshot of branch master taken on 2019-09-04T12:32:38.716-04:00 from "https://github.com/UnofficialJuliaMirror/X11_jll.jl-546b0b6d-9ca3-5ba2-8705-1bc1841d8479"

---
+++ src/X11_jll.jl
@@ -0,0 +1,26 @@
+module X11_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("546b0b6d-9ca3-5ba2-8705-1bc1841d8479"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "X11", artifacts_toml) for e in artifacts["X11"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load X11; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module X11_jll
